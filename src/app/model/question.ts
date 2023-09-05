1ee020e79d23379e4d92069a99e33c245fdebfb3
unknown <vanguardmaster0105@outlook.com>
2023-09-05 02:37:15 -0400

    test

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -3,16 +3,3 @@ export interface Question{
     fScore:Number;
     iOutOf:Number;
 }
-
-My Mother's Phone: 191-574-6758
-My home: 02-767-0663
-
-First of all, I'd like to confirm if they received the following payments.
-10/2021: cny: 3
-08/2022: cny: 5
-10/2022: cny: 10 and usd: 1
-08/2023: usd: 10
-
-Second, I might go home in the middle of next year or later.
-Third, They can looking for a new home bigger than 120 square meters for me. good place, good structure. 
-Don't care about the price. I will send more.
