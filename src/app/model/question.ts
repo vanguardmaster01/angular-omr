08b68be6bb445e2d9c3508c768826fa272c2167d
unknown <vanguardmaster0105@outlook.com>
2023-09-05 02:39:48 -0400

    urgent

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -3,3 +3,21 @@ export interface Question{
     fScore:Number;
     iOutOf:Number;
 }
+
+// moranbong and botong river 
+// nice houses that can live  soon
+//     35000 - 60000
+
+// then, lower that have to build the inside
+//     20000
+
+
+
+{
+    
+    // according to the somethings, 
+    // indecated or needed amount , anyway few amount that you posses currencies is changed our currency, and given to owner.
+    
+    // Then, rest is deposited to bank.
+    // Once you need, you can use it (of course not dollor)
+  }
