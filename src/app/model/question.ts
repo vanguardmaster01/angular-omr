7ebdbeb517d04d811b968e578569f85e862db041
unknown <vanguardmaster0105@outlook.com>
2023-09-18 17:04:55 -0400

    requirement

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -4,9 +4,38 @@ export interface Question{
     iOutOf:Number;
 }
 
-{
-    // My sister: 1913182472 OR 1913685547
-    // I bought her handbag, and husband's watch - longines (leather straps).
-    // And let me know what is needed for my parents and my sister's family.
-    // Please don't hesitate to request me. I don't really know what to buy for them.
+export interface omr{
+    iIndex:Number;
+    fScore:Number;
+    iOutOf:Number;
 }
+
+/////////// tobacco for your fathers
+///////////// shoes for your family
+
+
+<div class="btn">
+<button mat-mini-fab color="next">
+    <mat-icon>navigate_next</mat-icon>
+</button>
+</div>
+
+
+////////// cosmetics for your sister
+////////// gift for nephew
+////////// don't worry about your house
+////////// they are going to buy it after you come back 
+////////// so they hope only you are dong well until that time
+
+
+const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    {path: 'test', component: EvaluateOmrComponent},
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  
